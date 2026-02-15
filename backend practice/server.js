52849107b56af172d95e8b760ae6423222c65812
DanishNazakat <danishnazakat636@gmail.com>
2026-02-15 13:44:20 +0500

    jwt verification

--- backend practice/server.js
+++ backend practice/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cookieParser = require("cookie-parser")
+const authMiddleware = require("./middleware/verifyToken")
 require("dotenv").config();
 const dbConnection = require('./db/dbconnection')
 const app = express();
 const router = require('./router/route')
 app.use(cookieParser());
 app.use(express.json());
+app.use(authMiddleware);
 app.use('/api' , router)
 dbConnection();
 app.listen(process.env.PORT, ()=>{
