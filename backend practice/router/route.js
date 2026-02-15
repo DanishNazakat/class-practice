65e3f1a0a3d8da76461c3f94e4d0fcf16ee495d5
DanishNazakat <danishnazakat636@gmail.com>
2026-02-16 02:23:35 +0500

    add protected route

--- backend practice/router/route.js
+++ backend practice/router/route.js
@@ -4,12 +4,11 @@ const router = express.Router();
 const authMiddleware = require("../middleware/verifyToken")
 const {signup ,login} = require('../controllers/Auth');
 const {addProduct , getProduct} = require("../controllers/productController");
-const verifyToken = require("../middleware/verifyToken");
 
 
 router.post('/signup', signup);
 router.post('/login', login);
-router.post('/addProduct', addProduct)
+router.post('/addProduct', authMiddleware ,addProduct)
 router.post('/getProduct', getProduct)
 
 router.get("/dashboard",authMiddleware, (req, res) => {
