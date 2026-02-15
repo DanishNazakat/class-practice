6cf04f1aa035d1fac204f5adcab7596c55e4130f
DanishNazakat <danishnazakat636@gmail.com>
2026-02-16 02:03:05 +0500

    add verfication

--- backend practice/router/route.js
+++ backend practice/router/route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-
+const authMiddleware = require("../middleware/verifyToken")
 const {signup ,login} = require('../controllers/Auth');
 const {addProduct , getProduct} = require("../controllers/productController");
 const verifyToken = require("../middleware/verifyToken");
@@ -12,9 +12,17 @@ router.post('/login', login);
 router.post('/addProduct', addProduct)
 router.post('/getProduct', getProduct)
 
-router.get("/dashboard", verifyToken, (req, res) => {
+router.get("/dashboard",authMiddleware, (req, res) => {
   res.json({
     message: `Welcome ${req.user.id}, This is a protected dashboard route!`
+  });
+});
+
+
+
+router.get("/publicDashboard", (req, res) => {
+  res.json({
+    message: `Welcome  This is a public dashboard route!`
   });
 });
 module.exports = router
--- backend practice/server.js
+++ backend practice/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const cookieParser = require("cookie-parser")
-const authMiddleware = require("./middleware/verifyToken")
+// const authMiddleware = require("./middleware/verifyToken")
 require("dotenv").config();
 const dbConnection = require('./db/dbconnection')
 const app = express();
 const router = require('./router/route')
 app.use(cookieParser());
 app.use(express.json());
-app.use(authMiddleware);
 app.use('/api' , router)
+// app.use(authMiddleware);
 dbConnection();
 app.listen(process.env.PORT, ()=>{
     console.log(`server is running on PORT ${process.env.PORT}`);
