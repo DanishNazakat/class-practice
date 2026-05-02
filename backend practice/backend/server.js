d66cecf84608a17dea4f124d49ef7571b75dd48e
DanishNazakat <danishnazakat636@gmail.com>
2026-05-02 14:43:52 +0500

    add groq :

--- backend practice/backend/server.js
+++ backend practice/backend/server.js
@@ -4,9 +4,7 @@ const cookieParser = require("cookie-parser")
 require("dotenv").config();
 const dbConnection = require('./db/dbconnection')
 const app = express();
-const router = require('./router/route')
-app.use(cookieParser());
-app.use(express.json());
+
 app.use('/api' , router)
 // app.use(authMiddleware);
 dbConnection();
